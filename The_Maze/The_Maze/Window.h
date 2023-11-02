e0998f2b692649dc2564132b13f799ff9767c293
lenijer <70570095+lenijer@users.noreply.github.com>
2023-11-02 20:38:56 +0100

    removed redundant code

--- The_Maze/The_Maze/Window.h
+++ The_Maze/The_Maze/Window.h
@@ -1,7 +1,7 @@
 /*
 *       Made by: Lenita Njærheim
 *           Created On: 12/10-2023
-*           Completed: 12/10-2023
+*           Completed: 02/11-2023
 *       Project Name: The Maze
 *           File: Window.h
 *           turtoral used https://cplusplus.com/forum/windows/219154/
